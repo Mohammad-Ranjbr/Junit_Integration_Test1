09fe5581dfb66329a960b83f82a7036cdc4c4d6c
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-05-22 16:15:42 +0330

    First Commit

---
+++ src/test/java/com/example/SpringBootRestService/SpringBootRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
