ccff926bb11dadc69e221ff2b2dea927e679852e
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-05-24 18:57:25 +0330

    Importance of Mock Bean Annotation in mocking the external dependencies

--- src/test/java/com/example/SpringBootRestService/SpringBootRestServiceApplicationTests.java
+++ src/test/java/com/example/SpringBootRestService/SpringBootRestServiceApplicationTests.java
@@ -1,14 +1,34 @@
 package com.example.SpringBootRestService;
 
+import com.example.SpringBootRestService.controllers.LibraryController;
+import com.example.SpringBootRestService.models.Library;
+import com.example.SpringBootRestService.payloads.AddResponse;
+import com.example.SpringBootRestService.repositories.LibraryRepository;
 import com.example.SpringBootRestService.services.Implementation.LibraryServiceImpl;
 import com.example.SpringBootRestService.services.LibraryService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class SpringBootRestServiceApplicationTests {
 
+	private final LibraryController libraryController;
+	@MockBean
+	private LibraryService libraryService;
+	@MockBean
+	private LibraryRepository libraryRepository;
+
+	@Autowired
+	public SpringBootRestServiceApplicationTests(LibraryController libraryController){
+		this.libraryController = libraryController;
+	}
 	@Test
 	void contextLoads() {
 	}
@@ -20,6 +40,21 @@ class SpringBootRestServiceApplicationTests {
 		Assertions.assertEquals(id1,"OLD-ZMAN-10");
 		String id2 = libraryService.buildID("Book",11);
 		Assertions.assertEquals(id2,"Book-11");
+	}
+
+	@Test
+	public void addBookTest(){
+		Library library = new Library();
+		library.setAisle(20);
+		library.setName("Spring");
+		library.setIsbn("Book");
+		library.setAuthor("Mohammad Ranjbar");
+		library.setId("Book-20");
+		when(libraryService.buildID(library.getIsbn(),library.getAisle())).thenReturn(library.getId());
+		when(libraryService.checkBookAlreadyExist(library.getId())).thenReturn(false);
+		ResponseEntity<AddResponse> responseEntity = libraryController.addBook(library);
+		System.out.println(responseEntity.getStatusCode());
+		Assertions.assertEquals(responseEntity.getStatusCode(), HttpStatus.CREATED);
 	}
 
 }
