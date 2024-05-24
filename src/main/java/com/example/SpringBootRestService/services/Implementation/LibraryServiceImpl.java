85319afc38a2acdc1a54e0d1eb838afcfe313c68
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-05-24 16:37:54 +0330

    Identifying Right Candidates of Unit Testing and build testing approach

--- src/main/java/com/example/SpringBootRestService/services/Implementation/LibraryServiceImpl.java
+++ src/main/java/com/example/SpringBootRestService/services/Implementation/LibraryServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.SpringBootRestService.services.Implementation;
 
 import com.example.SpringBootRestService.repositories.LibraryRepository;
 import com.example.SpringBootRestService.services.LibraryService;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,8 @@ public class LibraryServiceImpl implements LibraryService {
 
     @Override
     public String buildID(String isbn, int aisle) {
+        if(isbn.startsWith("Z"))
+            return "OLD-" + isbn + "-" + aisle;
         return isbn + "-" + aisle;
     }
 
--- src/test/java/com/example/SpringBootRestService/SpringBootRestServiceApplicationTests.java
+++ src/test/java/com/example/SpringBootRestService/SpringBootRestServiceApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.SpringBootRestService;
 
+import com.example.SpringBootRestService.services.Implementation.LibraryServiceImpl;
+import com.example.SpringBootRestService.services.LibraryService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,15 @@ class SpringBootRestServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void checkBuildIDLogin(){
+		LibraryService libraryService = new LibraryServiceImpl(null);
+		String id1 = libraryService.buildID("ZMAN",10);
+		Assertions.assertEquals(id1,"OLD-ZMAN-10");
+		String id2 = libraryService.buildID("Book",11);
+		Assertions.assertEquals(id2,"Book-11");
 	}
 
 }
