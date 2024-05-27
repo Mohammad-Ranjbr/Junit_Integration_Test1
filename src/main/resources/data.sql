239df88f24d24fd7ff9e97219d4cbacf18ef9c2b
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-05-28 01:42:52 +0330

    Asserting the validations for response of APIServices with Integration Test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO library (book_name, id, isbn, aisle, author) VALUES ('Spring Boot', 'Book-20', 'Book', 20, 'Mohammad');
 INSERT INTO library (book_name, id, isbn, aisle, author) VALUES ('Spring Security', 'Book-21', 'Book', 21, 'Mohammad');
+INSERT INTO library (book_name, id, isbn, aisle, author) VALUES ('Spring Fundamentals', 'Book-22', 'Book', 22, 'Ali');
+
 
--- src/test/java/com/example/SpringBootRestService/SpringBootRestServiceIT.java
+++ src/test/java/com/example/SpringBootRestService/SpringBootRestServiceIT.java
@@ -1,6 +1,8 @@
 package com.example.SpringBootRestService;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,28 @@ public class SpringBootRestServiceIT { // this is a convention for set name to i
     //TestRestTemplate is default library spring boot gives all for integration test
 
     @Test
-    public void getBooksWithAuthorName(){
+    public void getBooksWithAuthorName() throws JSONException {
+        String expected = "[\n" +
+                "    {\n" +
+                "        \"id\": \"Book-20\",\n" +
+                "        \"aisle\": 20,\n" +
+                "        \"isbn\": \"Book\",\n" +
+                "        \"author\": \"Mohammad\",\n" +
+                "        \"name\": \"Spring Boot\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": \"Book-21\",\n" +
+                "        \"aisle\": 21,\n" +
+                "        \"isbn\": \"Book\",\n" +
+                "        \"author\": \"Mohammad\",\n" +
+                "        \"name\": \"Spring Security\"\n" +
+                "    }\n" +
+                "]";
         TestRestTemplate testRestTemplate = new TestRestTemplate();
         ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("http://localhost:8080/api/v1/getBooks/author/Mohammad",String.class);
         System.out.println(responseEntity.getStatusCode());
         System.out.println(responseEntity.getBody());
+        JSONAssert.assertEquals(expected,responseEntity.getBody(),false);
     }
 
 }
