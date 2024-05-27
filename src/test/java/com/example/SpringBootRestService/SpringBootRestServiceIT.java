105070f7f7a5c1e4a0e731d71505790078add945
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-05-28 02:12:51 +0330

    Build Integration Test for Post http method Rest Controller with assertions

--- src/test/java/com/example/SpringBootRestService/SpringBootRestServiceIT.java
+++ src/test/java/com/example/SpringBootRestService/SpringBootRestServiceIT.java
@@ -1,11 +1,15 @@
 package com.example.SpringBootRestService;
 
+import com.example.SpringBootRestService.models.Library;
 import org.json.JSONException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+
+import java.util.Objects;
 
 @SpringBootTest
 public class SpringBootRestServiceIT { // this is a convention for set name to integration test class with maven class name should end with IT
@@ -19,27 +23,50 @@ public class SpringBootRestServiceIT { // this is a convention for set name to i
 
     @Test
     public void getBooksWithAuthorName() throws JSONException {
-        String expected = "[\n" +
-                "    {\n" +
-                "        \"id\": \"Book-20\",\n" +
-                "        \"aisle\": 20,\n" +
-                "        \"isbn\": \"Book\",\n" +
-                "        \"author\": \"Mohammad\",\n" +
-                "        \"name\": \"Spring Boot\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "        \"id\": \"Book-21\",\n" +
-                "        \"aisle\": 21,\n" +
-                "        \"isbn\": \"Book\",\n" +
-                "        \"author\": \"Mohammad\",\n" +
-                "        \"name\": \"Spring Security\"\n" +
-                "    }\n" +
-                "]";
+        //Text Block
+        String expected = """ 
+                [
+                    {
+                        "id": "Book-20",
+                        "aisle": 20,
+                        "isbn": "Book",
+                        "author": "Mohammad",
+                        "name": "Spring Boot"
+                    },
+                    {
+                        "id": "Book-21",
+                        "aisle": 21,
+                        "isbn": "Book",
+                        "author": "Mohammad",
+                        "name": "Spring Security"
+                    }
+                ]""";
         TestRestTemplate testRestTemplate = new TestRestTemplate();
         ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("http://localhost:8080/api/v1/getBooks/author/Mohammad",String.class);
         System.out.println(responseEntity.getStatusCode());
         System.out.println(responseEntity.getBody());
         JSONAssert.assertEquals(expected,responseEntity.getBody(),false);
+    }
+
+    @Test
+    public void addBookIntegrationTest(){
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Library> request = new HttpEntity<>(buildlibrary(),httpHeaders);
+        ResponseEntity<String> responseEntity = testRestTemplate.postForEntity("http://localhost:8080/api/v1/addBook",request,String.class);
+        Assertions.assertEquals(HttpStatus.CREATED,responseEntity.getStatusCode());
+        Assertions.assertEquals(buildlibrary().getId(), Objects.requireNonNull(responseEntity.getHeaders().get("Unique")).get(0));
+    }
+
+    public Library buildlibrary(){
+        Library library = new Library();
+        library.setAisle(30);
+        library.setName("Spring");
+        library.setIsbn("Book");
+        library.setAuthor("Mohammad Ranjbar");
+        library.setId("Book-30");
+        return library;
     }
 
 }
