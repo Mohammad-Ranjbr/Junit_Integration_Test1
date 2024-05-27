ae96129e33ef78a055d3933a046b066e62e4fef0
Mohammad Ranjbar <mohammadranjbar.mmr81@gmail.com>
2024-05-28 01:04:33 +0330

    Build Mock SQL Properties file and feed with Mock data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO library (book_name, id, isbn, aisle, author) VALUES ('Spring Boot', 'Book-20', 'Book', 20, 'Mohammad');
+INSERT INTO library (book_name, id, isbn, aisle, author) VALUES ('Spring Security', 'Book-21', 'Book', 21, 'Mohammad');
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE library (
+     id VARCHAR(255) PRIMARY KEY,
+     book_name VARCHAR(255),
+     isbn VARCHAR(255),
+     aisle INT,
+     author VARCHAR(255)
+);
